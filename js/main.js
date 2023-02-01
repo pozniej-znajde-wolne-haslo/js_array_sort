a552045e12cac3036edda95dd5aee4901fe0cc38
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-02-01 13:37:23 +0100

    adds html- & js-file, exercise for array_sort()

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let languages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "Ruby",
+  "PHP",
+  "C++",
+  "CSS",
+  "C#",
+  "Go",
+  "C",
+  "TypeScript",
+  "Swift"
+];
+
+// sorts the elements alphabetically
+let languagesSorted = languages.sort();
+
+console.log(languagesSorted);
+
+
